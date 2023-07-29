2dae09bba6134b4af774009b27f0130875e94ce5
Alex Noppenberger <noppenbe@gmx.de>
2023-07-29 02:16:16 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('apiauth', [AuthController::class, 'signin']);
+Route::get('apiauth', [AuthController::class, 'signin']);
+Route::post('register', [AuthController::class, 'signup']);
+     
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('createOrder', ApiController::class);
+});
