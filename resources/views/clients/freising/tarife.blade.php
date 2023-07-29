57e45dd80fdb131e7e8a5965523ec187b2b88de6
Alex Noppenberger <noppenbe@gmx.de>
2023-07-29 17:12:16 +0200

    Client

--- resources/views/clients/freising/tarife.blade.php
+++ resources/views/clients/freising/tarife.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div id="tariffs w-full">
         <div class="content-wrapper content-odd fair">
-            <div class="content tariff">
+            <div class="content tariff relative">
                     <div>
                         <div class="headline mb-5">
                             <div><img class="w-12 h-auto mt-3 mr-4" src="{{asset('img/ckj13utoq0015ntxqoughe2ek-ckewz14od001kzsxq2b3nhal5-fair-ic.svg')}}"></div>
@@ -11,32 +11,40 @@
                             </div>
                             <div class="flex items-center justify-center text-2xl">
                                 <div>Student</div>
-                            </div>    
-                        </div>    
-                        <div class="text-black text-2xl mb-8">Einfach viele Vorteile</div>
-                        <div class="tariff-details">
-                            <div class="flex mb-5">
-                                <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Reduzierte Grundgebühr</div>
-                                <div class="text-black text-sm"><strong>Nur 11<sup>35</sup> Euro monatlich</strong></div>
-                            </div>    
-                            <div class="flex mb-5">
-                                <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Förderung</div>
-                                <div class="text-black text-sm">Förderung mit 050 netto / 060 brutto Cent / kWh</div>
                             </div>  
-                            <div class="flex mb-5">
-                                <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Verbrauchskosten</div>
-                                <div class="text-black text-sm">{{$_POST['usage']}} kWh × <span id="t1_wpb"></span> Cent / kWh = 1.265<sup>40</sup> Euro im Jahr</div>
-                            </div> 
-                            <div class="flex mb-5">
-                                <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Laufzeit</div>
-                                <div class="text-black text-sm">Mindestlaufzeit: 1 Monat</div>
-                            </div> 
-                            <div class="flex mb-5">
-                                <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Abschlag & Jahresenergiekosten</div>
-                                <div class="text-black text-sm">119<sup>30</sup> Euro pro Monat / 1.431<sup>60</sup> Euro im Jahr</div>
-                            </div> 
+                        </div>    
+                        <div class="text-black text-2xl mb-8">Einfach viele Vorteile</div>  
+                        <div class="relative">
+                            <div id="loader1" class="absolute z-10 w-full h-full flex bg-white bg-opacity-100 items-center justify-center">
+                                <img class="w-20 h-auto" src="{{asset('img/buffering-animated-text-icon-loading-u1h739who8u5mtw3.gif')}}">
+                            </div>
+                            
+                            <div class="tariff-details">
+                                <div class="flex mb-5">
+                                    <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Reduzierte Grundgebühr</div>
+                                    <div class="text-black text-sm"><strong>Nur 11<sup>35</sup> Euro monatlich</strong></div>
+                                </div>    
+                                <div class="flex mb-5">
+                                    <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Förderung</div>
+                                    <div class="text-black text-sm">Förderung mit 050 netto / 060 brutto Cent / kWh</div>
+                                </div>  
+                                <div class="flex mb-5">
+                                    <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Verbrauchskosten</div>
+                                    <div class="text-black text-sm">{{$_POST['usage']}} kWh × <span id="t1_wpb"></span><sup id="t1_wpb_dec"></sup> Cent / kWh = 1.265<sup>40</sup> Euro im Jahr</div>
+                                </div> 
+                                <div class="flex mb-5">
+                                    <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Laufzeit</div>
+                                    <div class="text-black text-sm">Mindestlaufzeit: 1 Monat</div>
+                                </div> 
+                                <div class="flex mb-5">
+                                    <div class="text-[#0ac] text-sm text-bold mr-10 w-1/4">Abschlag & Jahresenergiekosten</div>
+                                    <div class="text-black text-sm">119<sup>30</sup> Euro pro Monat / 1.431<sup>60</sup> Euro im Jahr</div>
+                                </div> 
+                            </div>
+                            <div>
+                                    <button class="btn-primary-odd">Jetzt bestellen</button>
+                            </div>    
                         </div>
-                        
                     </div>      
             </div>     
         </div>    
@@ -68,8 +76,11 @@
         $.ajax({
             url: "https://ww24pjl1v6.execute-api.eu-central-1.amazonaws.com/prod/tariffs?postcode="+zip+"&usage="+usage+"&business="+business+"&energy=electricity"
         }).done(function(resp) { 
-            var wpb = Math.round(resp.data['21_ftp_fair-ez']['workingPriceBrutto']).toFixed(2);
-            $('#t1_wpb').html(wpb);
+            var wpb = parseFloat(resp.data['21_ftp_fair-ez']['workingPriceBrutto']).toFixed(2);
+            wpb = wpb.split('.');
+            $('#t1_wpb').html(wpb[0]);
+            $('#t1_wpb_dec').html(wpb[1]);
+            $('#loader1').hide(200);
             console.log(resp.data['21_ftp_fair-ez']['workingPriceBrutto']);
             $('#tariffs').html(resp);
         },'json'); 
