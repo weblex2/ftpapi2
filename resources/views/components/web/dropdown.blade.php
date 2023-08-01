0daed5e8a267bd09e292a1542e19a43599ca12e6
Alex <noppenbe@gmx.de>
2023-08-01 15:21:47 +0200

    Update dropdown.blade.php

--- resources/views/components/web/dropdown.blade.php
+++ resources/views/components/web/dropdown.blade.php
@@ -1,6 +1,6 @@
 <div>
 
-    <select name="{{$name}}">
+    <select name="{{$name}}" {{ $attributes->merge(['class' => 'card']) }}>
     @foreach ($mydata as $key => $row)
     <option class="dropdown-option" value="{{$row['id']}}">{{$row['name']}}</option>
     @endforeach
