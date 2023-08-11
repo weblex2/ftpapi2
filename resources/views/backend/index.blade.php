b1ab57100bec807203284e088a747a3a7bc740eb
Alex Noppenberger <noppenbe@gmx.de>
2023-08-11 21:44:43 +0200

    Update index.blade.php

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,10 +1,26 @@
-<x-guest-layout>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-    <div class="grid grid-cols-2">
-        <div class="border border-slate-400 p-3 text-bold">Request</div> 
-        <div class="border border-slate-400 p-3 text-bold">Result</div>
-    @foreach($customerOrders as $i => $order)
-        <div class="border border-slate-400 p-3 break-words {{$i%2==0 ? "bg-slate-200" : ""}}">{{$order->order_detail}}</div>
-        <div class="border border-slate-400 p-3 break-words {{$i%2==0 ? "bg-slate-200" : ""}}">{{$order->result}}</div>
-    @endforeach
-</x-guest-layout>    
+    <div class="py-12 ">
+        <div class="max-w-[70%] mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="grid grid-cols-2">
+                    <div class="border border-slate-400 p-3 text-bold">Request</div> 
+                    <div class="border border-slate-400 p-3 text-bold">Result</div>
+                @foreach($customerOrders as $i => $order)
+                    <div class="border border-slate-400 p-3 break-words {{$i%2==0 ? "bg-slate-200" : ""}}">{{$order->order_detail}}</div>
+                    <div class="border border-slate-400 p-3 break-words {{$i%2==0 ? "bg-slate-200" : ""}}">{{$order->result}}</div>
+                @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+
+
+  
