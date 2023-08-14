94b342a436ab2a0b0ee731ff2d7489cc26f52e27
Alex Noppenberger <noppenbe@gmx.de>
2023-08-14 21:22:52 +0200

    Update about.blade.php

--- resources/views/clients/freising/about.blade.php
+++ resources/views/clients/freising/about.blade.php
@@ -11,7 +11,7 @@
 
 <div class="content-wrapper">
     <div class="content leading-7">
-        <h2 class="my-10  text-[#0ac]">Fair Trade Power ist der erste Stromanbieter in Verantwortungseigentum und fühlt sich geehrt, künftig Einrichtungen der katholischen Kirche in Bayern mit echtem Ökostrom beliefern zu dürfen.</h2>
+        <h2 class="my-10  text-[#0ac] text-left">Fair Trade Power ist der erste Stromanbieter in Verantwortungseigentum und fühlt sich geehrt, künftig Einrichtungen der katholischen Kirche in Bayern mit echtem Ökostrom beliefern zu dürfen.</h2>
         <p>
             In welcher Hinsicht wir uns den christlichen Werten Ihrer Einrichtung verbunden fühlen und was diese Werte mit dem Strommarkt zu tun haben, möchten wir in dieser Vorbemerkung darlegen.
         </p>
@@ -21,7 +21,7 @@
             Ökostrom bei Fair Trade Power zu beziehen, bedeutet nicht nur den ökologischen Fußabdruck Ihrer Einrichtung deutlich zu reduzieren, sondern auch, soziale Verantwortung zu übernehmen. Denn mit dem echten Ökostrom von Fair Trade Power werden keine Profitinteressen von Aktionären oder Eigentümern bedient, denn Fair Trade Power gehört sich selbst. Alle Gewinne, die Fair Trade Power macht, werden in neue Erzeugungsanlagen für erneuerbare Energien reinvestiert. Damit bringen wir proaktiv die Energiewende voran und sind gleichzeitig ein Vorreiter für soziales und solidarisches Wirtschaften.
         </p>
 
-        <h2 class="my-10  text-[#0ac]">Fair Trade Power ist der einzige Ökostromanbieter in Verantwortungseigentum. Was bedeutet das?</h2>
+        <h2 class="my-10  text-[#0ac] text-left">Fair Trade Power ist der einzige Ökostromanbieter in Verantwortungseigentum. Was bedeutet das?</h2>
         <p>
             Als Unternehmen der „Purpose Economy“ wird sichergestellt, dass der Geschäftszweck der Firma festgeschrieben bleibt, keine Spekulationsgewinne an Anteilseigner oder Investoren ausgeschüttet werden können und dass das Unternehmen nicht zu „Greenwashing“-Zwecken verkauft werden kann. Mehr zum Thema Verantwortungseigentum finden Sie auf der FTP-Webseite https://www.fairtradepower.de/purpose/
         </p>
